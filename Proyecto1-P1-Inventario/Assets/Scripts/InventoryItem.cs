5fa6bd9d94f3e629e59e491ece2ad435eb3ff4cd
Jeshua Cienfuegos Estrada <Deku050204@gmail.com>
2024-02-07 14:41:36 -0600

    Se Realizo la prueba del Drag-(Se logro))
    Se realizo un intento de realizar drag en el inventario y funciono.
    
    Se agregaron comentarios en las funciones del script para explicar su funcionamiento.

--- Proyecto1-P1-Inventario/Assets/Scripts/InventoryItem.cs
+++ Proyecto1-P1-Inventario/Assets/Scripts/InventoryItem.cs
@@ -9,30 +9,41 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     [Header("UI")]
     public Image image;
 
+    [HideInInspector] public Transform parentAfterDrag;
+
+
+    //Se crea la función que inicia el drag- mandando un mensaje que se inicio el drag.
+    // Raycast se desactiva para poder mover los iconos por el tolbar.
+    //Se hace parent para que el objeto que se selecciona pueda moverse por encima de la UI.
     public void OnBeginDrag(PointerEventData evenData)
     {
-        image.raycastTarget = false;
 
-        //Debug.Log("comenzo el drag");
-        //parentAfterDrag = transform.parent;
-        //transform.SetParent(transform.root);
+        Debug.Log("comenzo el drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        image.raycastTarget = false;
         //transform.SetAsLastSibling();
         
     }
 
+
+    //Funcion que mantiene el objeto mientras el mouse mantiene su click.
+    //Manda mensaje de que se está haciendo Dragging.
     public void OnDrag(PointerEventData evenData)
     {
+        Debug.Log("Dragging");
         transform.position = Input.mousePosition;
-
-        //Debug.Log("Dragging");
     }
 
+
+    // Raycast se activa para poder colocar la imagen en el tolbar.
+    //Cuando se termina el drag- se hace parent del slot para colocarse por encima de este.
     public void OnEndDrag(PointerEventData eventData)
     {
-        image.raycastTarget = true;
 
-        //Debug.Log("termino el drag");
-        //transform.SetParent(parentAfterDrag);
+        Debug.Log("termino el drag");
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
        
     }
 }
